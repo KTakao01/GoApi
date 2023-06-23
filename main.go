7ed15af644def4222efc4dd1344dc088ef62c47a
小泉孝雄 <takoizmi22@gmail.com>
2023-06-23 14:03:15 +0900

    ハンドラ追加~p42

--- main.go
+++ main.go
@@ -6,13 +6,41 @@ import (
 	"net/http"
 )
 
-func main(){
+func main() {
+	//ハンドラの定義
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w,"Hello,world!\n")
+		io.WriteString(w, "Hello,world!\n")
+	}
+	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Article...\n")
+	}
+	articleListHandler := func(w http.ResponseWriter,req *http.Request){
+		io.WriteString((w,"Article List\n"))
 	}
 
-	http.HandleFunc("/",helloHandler)
+	articleDetailHandler := func(w http.ResponseWriter,req *http.Request){
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n",articleID)
+		io.WriteString(w,resString)
+	}
+
+	postNiceHandler := func(w http.ResponseWriter,req *http.Request){
+		io.WriteString(w,"Posting Nice...\n")
+	}
+
+	postCommentHandler := func(w http.ResponseWriter,req *http.Request){
+		io.WriteString(w, "Posting Comment...\n")
+	}
+
+	//定義したハンドラの登録
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1",articleDetailHandler)
+	http.HandleFunc("/article/nice",postNiceHandler)
+	http.HandleFunc("/comment",postCommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	//サーバー起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
