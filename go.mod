0d653a4da607878638ee1df38bf02faa43354345
小泉孝雄 <takoizmi22@gmail.com>
2023-07-02 15:56:04 +0900

    1_5_演習問題

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/KTakao01/GoApi
 
-go 1.18
+go 1.2
 
 require github.com/gorilla/mux v1.8.0 // indirect
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,29 +4,38 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // ハンドラの定義
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello,world!\n")
+	io.WriteString(w, "Hello,world!\n")
 }
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Article...\n")
+	io.WriteString(w, "Posting Article...\n")
 }
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Article List\n")
+	io.WriteString(w, "Article List\n")
 }
 
+// GET /article/{id}のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
+	idParam := mux.Vars(req)["id"]
+	articleID, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Invalid query parameter: %v", idParam), http.StatusBadRequest)
+		return
+	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Nice...\n")
+	io.WriteString(w, "Posting Nice...\n")
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Comment...\n")
+	io.WriteString(w, "Posting Comment...\n")
 }
