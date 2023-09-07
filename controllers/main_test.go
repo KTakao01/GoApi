f15283ae51536b5a72df9b1bc2f6d88b7c1de93b
KTakao01 <takoizmi22@gmail.com>
2023-09-07 18:42:08 +0900

    8-2

--- controllers/main_test.go
+++ controllers/main_test.go
@@ -3,24 +3,29 @@ package controllers_test
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 	"testing"
 
 	"github.com/KTakao01/GoApi/controllers"
 	"github.com/KTakao01/GoApi/services"
-	_ "github.com/go-sql-driver//mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // 1.テストに使うリソース（コントローラ構造体)を用意
 var aCon *controllers.ArticleController
 
 func TestMain(m *testing.M) {
-	dbUSer := "docker"
+	dbUser := "docker"
 	dbPassword := "docker"
 	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser,
-		dbPassword, dbDatabase)
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 
 	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("DB setup fail")
+		os.Exit(1)
+	}
 	ser := services.NewMyAppService(db)
 	aCon = controllers.NewArticleController(ser)
 
